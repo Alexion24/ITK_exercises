3cbbb3bb4082ab80dbe284333feaf56c3b852e3f
Alexion <alex24bryant@mail.ru>
2025-09-10 18:31:15 +0500

    feat: add 2 alg exercises

---
+++ src/algs/remove_duplicates.py
@@ -0,0 +1,19 @@
+NUMS = [1, 1, 2, 2, 3, 4, 4, 5]
+
+
+def remove_duplicates(nums: list[int]) -> tuple[int, list[int]]:
+    if not nums:
+        return 0, []
+
+    # указатель на последний уникальный элемент
+    last_idx = 0
+    for new_idx in range(1, len(nums)):
+        if nums[new_idx] != nums[last_idx]:
+            last_idx += 1
+            nums[last_idx] = nums[new_idx]
+    # Длина уникальных элементов - last_idx + 1
+    return last_idx + 1, nums[: last_idx + 1]
+
+
+k, output = remove_duplicates(NUMS)
+print(k, output)
---
+++ src/algs/two_sum.py
@@ -0,0 +1,14 @@
+NUMS = [2, 7, 11, 15]
+TARGET = 9
+
+
+def find_two_sum(nums: list, target: int) -> list:
+    num_to_index = {}
+    for idx, num in enumerate(nums):
+        if target - num in num_to_index:
+            return [num_to_index[target - num], idx]
+        num_to_index[num] = idx
+    return []
+
+
+print(find_two_sum(NUMS, TARGET))
